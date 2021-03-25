c755b806bcb968e9b8c79e6ee022480ce524e72e
alvynah <alvynahw@gmail.com>
2021-03-26 00:58:50 +0300

    Add component to display form for inputting new quotes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
