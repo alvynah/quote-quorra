f6b6eb4db06f0954aeef908d3b83be8497633fac
alvynah <alvynahw@gmail.com>
2021-03-25 22:23:40 +0300

    Add initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/quotes.spec.ts
@@ -0,0 +1,7 @@
+import { Quotes } from './quotes';
+
+describe('Quotes', () => {
+  it('should create an instance', () => {
+    expect(new Quotes()).toBeTruthy();
+  });
+});
