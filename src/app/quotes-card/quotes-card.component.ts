4a778b13ec98cdaebafd07e5b0eaddfb20562926
alvynah <alvynahw@gmail.com>
2021-03-28 07:59:28 +0300

    Add the likes and dislikes after button and indicate an initial value of 0

--- src/app/quotes-card/quotes-card.component.ts
+++ src/app/quotes-card/quotes-card.component.ts
@@ -7,10 +7,11 @@ import { Quotes} from '../quotes';
 })
 export class QuotesCardComponent implements OnInit {
   quotes: Quotes[] = [
-    new Quotes(1, 'Alvynah', 'Challenges', 'The greatest glory in living lies not in never falling, but in rising every time we fall.', 'Nelson Mandela', new Date(2019, 3, 23)),
-    new Quotes(2, 'Alvynah', 'Work', 'The way to get started is to quit talking and begin doing', 'Walt Disney', new Date(2020, 6, 30)),
-    new Quotes(3, 'Alvynah', 'Uncertainity', 'If life were predictable it would cease to be life, and be without flavor.', 'Eleanor Roosevelt', new Date(2016, 4, 13)),
-    new Quotes(4, 'Alvynah', 'Life', 'Live, Laugh, Love', 'Alvynah Wabwoba', new Date(2021, 3, 26)),
+    new Quotes(1, 'Alvynah', 'Challenges', 'The greatest glory in living lies not in never falling, but in rising every time we fall.', 'Nelson Mandela', new Date(2019, 3, 23), 0, 0),
+    // tslint:disable-next-line:max-line-length
+    new Quotes(2, 'Alvynah', 'Work', 'The way to get started is to quit talking and begin doing', 'Walt Disney', new Date(2020, 6, 30), 0, 0),
+    new Quotes(3, 'Alvynah', 'Uncertainity', 'If life were predictable it would cease to be life, and be without flavor.', 'Eleanor Roosevelt', new Date(2016, 4, 13), 0, 0),
+    new Quotes(4, 'Alvynah', 'Life', 'Live, Laugh, Love', 'Alvynah Wabwoba', new Date(2021, 3, 26), 0, 0),
   ];
   // tslint:disable-next-line:typedef
   quoteDelete(isDone: boolean, index: number){
--- src/app/vote-details/vote-details.component.ts
+++ src/app/vote-details/vote-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-vote-details',
@@ -6,13 +7,20 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./vote-details.component.css']
 })
 export class VoteDetailsComponent implements OnInit {
+ @Input()
+  quote!: Quotes;
 @Output() isDone = new EventEmitter<boolean>();
 
 // tslint:disable-next-line:typedef
 quoteDelete(complete: boolean){
   this.isDone.emit(complete);
 }
-
+upVote(){
+  this.quote.likes += 1;
+}
+downVote(){
+  this.quote.dislikes += 1;
+}
   constructor() { }
 
   ngOnInit(): void {
