6c358e1ab36e5432bc97e7b41a5fc4ffa0f45ded
alvynah <alvynahw@gmail.com>
2021-03-28 16:42:40 +0300

    Reset form on submit, display each card on its own

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core'
+import { NgForm } from '@angular/forms';
 import { Quotes} from '../quotes';
 
 @Component({
@@ -11,8 +12,11 @@ newQuote = new Quotes(0, '', '', '', '', new Date(), 0, 0);
 @Output() addQuote = new EventEmitter<Quotes>();
 // tslint:disable-next-line:typedef
 submitQuote(){
-  
+
   this.addQuote.emit(this.newQuote);
+  console.log(this.newQuote);
+  this.newQuote = new Quotes(0, '', '', '', '', new Date(), 0, 0);
+
 }
   constructor() { }
 
--- src/app/quotes-card/quotes-card.component.ts
+++ src/app/quotes-card/quotes-card.component.ts
@@ -10,22 +10,24 @@ export class QuotesCardComponent implements OnInit {
     new Quotes(1, 'Alvynah', 'Challenges', 'The greatest glory in living lies not in never falling, but in rising every time we fall.', 'Nelson Mandela', new Date(2019, 3, 23), 0, 0),
     // tslint:disable-next-line:max-line-length
     new Quotes(2, 'Alvynah', 'Work', 'The way to get started is to quit talking and begin doing', 'Walt Disney', new Date(2020, 6, 30), 0, 0),
-    new Quotes(3, 'Alvynah', 'Uncertainity', 'If life were predictable it would cease to be life, and be without flavor.', 'Eleanor Roosevelt', new Date(2016, 4, 13), 0, 0),
-    new Quotes(4, 'Alvynah', 'Life', 'Live, Laugh, Love', 'Alvynah Wabwoba', new Date(2021, 2, 28), 0, 0),
+    new Quotes(3, 'Alvynah', 'Life', 'Live, Laugh, Love', 'Alvynah Wabwoba', new Date(2021, 2, 28), 0, 0),
   ];
   // tslint:disable-next-line:typedef
   addNewQuote(quote: Quotes) {
-    let quoteLength = this.quotes.length;
+    const quoteLength = this.quotes.length;
     quote.id = quoteLength + 1;
     quote.dateCreated = new Date(quote.dateCreated);
-    
     this.quotes.push(quote);
-
   }
+
   // tslint:disable-next-line:typedef
   quoteDelete(isDone: boolean, index: number){
     if (isDone) {
+      const toDeleteQuote = confirm('Are you sure you want to delete?');
+      if (toDeleteQuote){
       this.quotes.splice(index, 1);
+
+    }
     }
   }
 
